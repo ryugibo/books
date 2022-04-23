bdab17d1a36d24f7fdd1759e1ffe4407cbe504d5
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-23 14:46:40 +0000

    Deploying to gh-pages from @ ryugibo/books@0ce5f616b72e863e704738fecf2782e0d714d7bf 🚀

---
+++ _next/static/y-HuBrKWm2tlq5M1P1UUA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Flog","\u002Ftags","\u002Fabout","\u002Flog\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]","\u002Flog\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
